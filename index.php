25591ec0af514b7cf3a4ffb0e572cc8260862a7b
Aulia-dst <dastiaulia@gmail.com>
2025-04-24 10:43:31 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"content="width-device-width, initial-scale=1.0">
+        <title>Latihan</title>
+    </head>
+    <body>
+        <h1>LATIHAN DASAR</h1>
+        <p>REPOSITORY GITHUB</p>
+    </body>    
+</html>
